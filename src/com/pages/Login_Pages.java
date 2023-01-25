e2e2721d1263d21e3753c6fca80c42ce8f803cf5
Ajim Sheikh <nagpurkhan123@gmail.com>
2023-01-25 09:55:57 -0800

    second commit

--- src/com/pages/Login_Pages.java
+++ src/com/pages/Login_Pages.java
@@ -1,5 +1,11 @@
 package com.pages;
 
 public class Login_Pages {
+	
+	public void LoginPage()
+	{
+		System.out.println("This is a login Page");
+		System.out.println("This is a new comment");
+	}
 
 }
